50c17a1e30c54b57fe28b6ada93b1a4a339da7e1
Artur Symanovic <artur.symanovic@gmail.com>
2020-09-03 22:50:43 +0100

    Added search model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,12 +1,4 @@
-import axios from 'axios';
+import Search from './models/Search'
 
-async function getResults(query){
-    try {   
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    } catch (error) {
-        console.log(error);
-    }
-}
-getResults('pizza');
+const search = new Search('pizza');
+search.getResults();
