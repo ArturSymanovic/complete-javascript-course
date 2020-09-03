d6fdc0d69eb335b1710cc026cbe0d12a6afcbec9
Artur Symanovic <artur.symanovic@gmail.com>
2020-09-03 13:21:30 +0100

    Added mvc structure and played around with import/export modules functionality

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,5 @@
 // Global app controller
+import str from './models/Search';
+//import {add as a, multiply, ID } from './views/SearchView';
+import * as searchView from './views/SearchView';
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
