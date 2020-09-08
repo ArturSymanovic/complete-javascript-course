a229548b0377d1442d2b4f8c649057c8d97c1cce
Artur Symanovic <artur.symanovic@gmail.com>
2020-09-08 14:24:09 +0100

    Added updating recipe servings functionality

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -99,7 +99,21 @@ const controlRecipe = async () => {
     }
 };
 
-// window.addEventListener('hashchange', controlRecipe);
-// window.addEventListener('load', controlRecipe);
-// Same as above
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease btn is clicked 
+        if (state.recipe.servings > 1 ) {
+            state.recipe.updateServings('dec');    
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')){
+        // Increase btn is clicked 
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+});
+
+
